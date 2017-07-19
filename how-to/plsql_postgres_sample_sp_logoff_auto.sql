3b09b7967fb88455a8e63a1797e3ae907b6fe3c3
Igor Takats <igor.takats@gmail.com>
2017-07-19 13:35:27 -0300

    Copiado o diretorio how-to do GRTP

---
+++ how-to/plsql_postgres_sample_sp_logoff_auto.sql
@@ -0,0 +1,20 @@
+﻿-- Function: sp_logoff_auto()
+
+-- DROP FUNCTION sp_logoff_auto();
+
+CREATE OR REPLACE FUNCTION sp_logoff_auto()
+  RETURNS void AS
+$BODY$  declare  
+    r requisicao%rowtype;
+begin
+         for r in select *
+	from requisicao
+	where ((current_timestamp - tslogin) >= interval '24 hours') and (tslogoff is null) loop
+	update requisicao set tslogoff = to_timestamp('01 jan 3000', 'dd mon yyyy') where id = r.id;
+      end loop;	
+
+end;$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION sp_logoff_auto()
+  OWNER TO postgres;
